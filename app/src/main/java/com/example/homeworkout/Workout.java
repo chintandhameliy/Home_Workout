052c8d018605269ddc03188563b8b937a62f6abb
Jaimish Dholariya <jdholariya374@rku.ac.in>
2023-01-03 17:36:57 +0530

    all

--- app/src/main/java/com/example/homeworkout/Workout.java
+++ app/src/main/java/com/example/homeworkout/Workout.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Workout extends AppCompatActivity {
 
-    CardView fullbody;
+    CardView fullbody,arm,abs,chest,leg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+
+        fullbody = findViewById(R.id.fullbody);
+        arm = findViewById(R.id.arm);
+        abs = findViewById(R.id.abs);
+        chest = findViewById(R.id.chest);
+        leg = findViewById(R.id.leg);
+
+
     }
 }
